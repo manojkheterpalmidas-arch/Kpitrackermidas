d9c2c3b2ab293a3d92f8ed3596e990fdd3a34e8d
Manoj <manojkheterpal.midas@gmail.com>
2026-06-29 15:36:33 +0200

    Normalize Vercel table row params

--- api/table/[table]/[id].js
+++ api/table/[table]/[id].js
@@ -1,5 +1,12 @@
 import { requestHandler } from "../../../dist/server/index.js";
 
 export default function handler(req, res) {
+  const table = first(req.query?.table);
+  const id = first(req.query?.id);
+  if (table && id) req.url = `/api/table/${encodeURIComponent(table)}/${encodeURIComponent(id)}`;
   return requestHandler(req, res);
+}
+
+function first(value) {
+  return Array.isArray(value) ? value[0] : value;
 }
